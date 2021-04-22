8bd3a313e09a792f1c8dbf2f23e577817fb59807
Jeffrey Platt <JSPlatt@gmail.com>
2021-04-22 15:09:09 -0400

    styling started

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-
+import './Header.css'
 
 
 function Header() {
     return (
-        <div>
+    <div>
+        <div className="header">
             <div className="header_buttons">
-            < Link to="/pictures">Pictures</Link> 
+            < Link to="/pictures">Pictures</Link> <br>
+            </br>
             < Link to="/friends">Friends</Link> 
             </div>
             <h1>Welcome to DogGO!</h1>
         </div>
+    </div>
     )
 }
 
